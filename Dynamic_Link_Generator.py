53833b49812b2a4937f4b6e046167f69b5f01c59
RagnarokEnder28 <58660692+RagnarokEnder28@users.noreply.github.com>
2026-06-23 10:25:56 +0800

    Update Dynamic_Link_Generator.py

--- Dynamic_Link_Generator.py
+++ Dynamic_Link_Generator.py
@@ -54,8 +54,8 @@ else:
         except:
             form_type_raw = "EXTERNAL"
 
-        EXTERNAL_FORM = "https://forms.office.com/r/6Tj43R5Ljr?embed=true"
-        INTERNAL_FORM = "https://forms.office.com/r/zCAJQxQ47x?embed=true"
+        EXTERNAL_FORM = "https://forms.office.com/r/KchEak7FWA?embed=true"
+        INTERNAL_FORM = "https://forms.office.com/r/5s3GA7Df0T?embed=true"
 
         # --- SINGLE LOGIC GATEWAY ---
         if current_status == "Active":
